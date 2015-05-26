31a5dcc956ff7e09870f2f13d47cf802d6914fde
Cloudox <cloudox@foxmail.com>
2015-05-26 16:54:19 +0800

    Initial Commit

---
+++ ViewDeckTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewDeckTest
+//
+//  Created by mac on 15/5/26.
+//  Copyright (c) 2015年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewDeckTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewDeckTest
+//
+//  Created by mac on 15/5/26.
+//  Copyright (c) 2015年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
